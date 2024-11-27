767c5918b88a301fecca26c490923744d2342d73
isaac <minimaleffort769@gmail.com>
2024-11-27 02:23:12 +0000

    fix: minor typo

--- conf.d/eza.fish
+++ conf.d/eza.fish
@@ -1,5 +1,5 @@
 if status is-interactive; and type -q eza
-  function ls --wraps=exa --wraps='eza --group-directories-first' --description 'alias ls eza --group-directories-first'
+  function ls --wraps=eza --wraps='eza --group-directories-first' --description 'alias ls eza --group-directories-first'
     eza --group-directories-first $argv
   end
 end
