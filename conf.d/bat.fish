92cb9e16ba57c68c6bece6549aca403d53d4d9c3
isaac <minimaleffort769@gmail.com>
2025-02-21 22:25:26 +0700

    feat(bat): plain style when using bat for tailing files

--- conf.d/bat.fish
+++ conf.d/bat.fish
@@ -1,5 +1,5 @@
 if status is-interactive; and type -q bat; and type -q tail
   function batwatch --description 'alias batwatch tail -f $FILE | bat --paging=never -l log'
-    tail -f $argv[1] | bat --paging=never -l log
+    tail -f $argv[1] | bat --paging=never --style=plain -l log
   end
 end
