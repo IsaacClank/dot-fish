73caf55bf7ada9016943c0409ce463764c7aec57
Isaac <minimaleffort769@gmail.com>
2026-02-25 17:52:03 +0700

    refactor(binding): clearer binding

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -2,6 +2,6 @@ if status is-interactive; and type -q fzf
   function fish_user_key_bindings
     fzf --fish | source
 
-    bind -M default \cg\cs 'commandline; git_switch_fzf; commandline -f repaint'
+    bind -M default ctrl-g,ctrl-s 'commandline; git_switch_fzf; commandline -f repaint'
   end
 end
