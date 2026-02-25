e8beef50d824072d30642c1c52916ed04612b43f
Isaac <minimaleffort769@gmail.com>
2026-02-25 17:09:22 +0700

    feat(bitwarden): utilities wrapped over bw

---
+++ conf.d/bitwarden.fish
@@ -0,0 +1,16 @@
+if status is-interactive;
+  function bw --wraps='flatpak run --command=bw com.bitwarden.desktop' --description 'alias bw flatpak run --command=bw com.bitwarden.desktop'
+      flatpak run --command=bw com.bitwarden.desktop $argv
+  end
+
+  function bw-sync
+    bw sync
+    set -Ux BW_ITEMS (bw list items)
+  end
+
+  function bw-get-pass
+    set -l selected_item (echo $BW_ITEMS | jq -r 'map({name, "username": (.login.username)} | "\\(.name) | \\(.username)")[]' | fzf)
+    set -l selected_item_segments (string split ' | ' $selected_item)
+    echo $BW_ITEMS | jq -r "map(select(.name == \"$selected_item_segments[1]\" and .login.username == \"$selected_item_segments[2]\") | .login.password) | first" | fish_clipboard_copy
+  end
+end
