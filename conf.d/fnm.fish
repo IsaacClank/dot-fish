ce469a8313a58962daa29f205eeb4795da554201
Isaac <minimaleffort769@gmail.com>
2026-03-22 12:28:56 +0700

    fix(fnm): incorrect fnm detection

--- conf.d/fnm.fish
+++ conf.d/fnm.fish
@@ -1,12 +1,9 @@
 if status is-interactive; and type -q fnm;
-  fnm env --use-on-cd --shell fish | source
+  set FNM_PATH "$HOME/.local/share/fnm"
+  if [ -d "$FNM_PATH" ]
+    set PATH "$FNM_PATH" $PATH
+    fnm env | source
+    fnm completions --shell fish | source
+  end
 end
 
-
-# fnm
-set FNM_PATH "$HOME/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-  fnm env | source
-  fnm completions --shell fish | source
-end
