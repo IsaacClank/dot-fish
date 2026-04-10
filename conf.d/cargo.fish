27285de9c7a8009489b38b78c62b8648082b553a
Isaac <minimaleffort769@gmail.com>
2026-04-10 14:31:37 +0700

    refactor(rust): update sourcing

--- conf.d/cargo.fish
+++
@@ -1,5 +0,0 @@
-if status is-interactive; and type -q cargo;
-  if test -d ~/.cargo/bin
-    fish_add_path -g ~/.cargo/bin
-  end
-end
---
+++ conf.d/rust.fish
@@ -0,0 +1,5 @@
+if status is-interactive;
+  if test -f ~/.cargo/env.fish
+    source ~/.cargo/env.fish
+  end
+end
